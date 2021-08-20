918c75c5735866f7865ff175b172c681e9bfc819
woonerb <woonerb@daum.net>
2021-08-20 22:32:42 +0900

    주석 보완 및 불필요 코드 삭제

--- app.js
+++ app.js
@@ -23,39 +23,32 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-// app.get('/', (req, res) => {
-//   var sql = "SELECT * FROM users";
-//   con.query(sql, function (err, result, fields) {
-//     if (err) throw err;
-//     res.send(result);
-//     });
-// });
-
 //사용자 목록을 보여준다. (초기화)
 app.get('/', (req, res) => {
     var sql = "SELECT * FROM users";
     con.query(sql, function (err, result, fields) {
       if (err) throw err;
-      // console.log(result);
       res.render('index',{users : result});
     });
 });
 
+
+//INSERT
 //신규 입력 양식을 보여준다.
 app.get('/insert', (req, res) => {res.sendFile(path.join(__dirname, 'html/form.html'))});
 
 //신규 사용자 정보를 테이블에 입력하고 초기 화면으로 리다이렉트 한다.
 app.post('/', (req, res) => {
    var sql = "INSERT INTO users SET ?";
-   // console.log(req.body);
+
    con.query(sql, req.body, function (err, result, fields) {
+
      if (err) throw err;
-     // console.log(result);
-     // res.send('입력되었습니다.');
      res.redirect('/'); //초기 목록 화면으로 리다이렉트
      });
 });
 
+//DELETE
 app.get('/delete/:id', (req, res) => {
   var sql = "DELETE FROM users WHERE id = ?";
   con.query(sql, [req.params.id], function (err, result, fields) {
@@ -65,6 +58,7 @@ app.get('/delete/:id', (req, res) => {
     });
 });
 
+//변경대상 찾기
 app.get('/edit/:id', (req, res) => {
   var sql = "SELECT * FROM users WHERE id = ?";
   con.query(sql, [req.params.id], function (err, result, fields) {
@@ -74,6 +68,7 @@ app.get('/edit/:id', (req, res) => {
     });
 });
 
+//변경대상 UPDATE
 app.post('/update/:id', (req, res) => {
   var sql = "UPDATE users SET ? WHERE id = " + req.params.id;
   console.log(req.body);
@@ -83,6 +78,4 @@ app.post('/update/:id', (req, res) => {
     });
 });
 
-app.get('/summer', (req, res) => res.send('아직 여름입니다.'))
-app.get('/winter', (req, res) => res.send('겨울에는 추워요!!'))
 app.listen(WEB_SERVER_PORT, () => console.log('서버가 시작되었습니다.'))
